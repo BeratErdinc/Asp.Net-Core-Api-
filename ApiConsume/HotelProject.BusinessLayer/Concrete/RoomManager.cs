d02eddd056cc6e5043db341a9531329ac3c8fee8
BERAT ERDİNÇ <202516044@ogr.cbu.edu.tr>
2023-09-27 18:48:22 +0300

    Business Layerın classları tanımlandı

---
+++ ApiConsume/HotelProject.BusinessLayer/Concrete/RoomManager.cs
@@ -0,0 +1,38 @@
+﻿using HotelProject.BusinessLayer.Abstract;
+using HotelProject.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.BusinessLayer.Concrete
+{
+    public class RoomManager : IRoomService
+    {
+        public void TDelete(Room t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Room TGetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Room> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TInsert(Room t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Room t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
